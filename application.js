77c663c30221a127d5b998ccd5f9a79ba65f82c5
Roni769361 <60136750+Roni769361@users.noreply.github.com>
2022-08-31 20:41:29 +0600

    first commit

---
+++ application.js
@@ -0,0 +1,20 @@
+// function functionWarkNow(){
+//     // console.log("Click Marce reee Baba")
+//     const num = document.getElementById("valueadd").value;
+//     const add = num++;
+//     console.log(add);
+// }
+
+function incrementValue(){
+    var value = parseInt(document.getElementById('valueadd').value,);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    // console.log(value);
+    document.getElementById('valueadd').value = value;
+}
+
+function amas(){
+    // console.log("click marce")
+    let item = parseInt (document.getElementById())
+
+}
