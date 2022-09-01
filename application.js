4c4da3539b89ef3c1c9ebe7cee9a1697940d59db
Ovimonno Chy Roni <oceanc9361@gmail.com>
2022-09-01 09:32:27 +0600

    last

--- application.js
+++ application.js
@@ -1,19 +1,30 @@
 
+// function incrementValue(){
+//     const inputtxt = document.getElementById("inputtext").value;
+//     if(inputtxt == ""){
+//         alert("Please Enter Your City Name.")
+//     }
+//     else{
+//         let item = parseInt (document.getElementById("valueadd").value);
+//         item = item +1
+//         document.getElementById("valueadd").value = item;
+//         document.getElementById("inputtext").value = "";   
+//     }
+// }
+
 
 function incrementValue(){
-    const inputtxt = document.getElementById("inputtext").value;
-    if(inputtxt == ""){
-        alert("Please Enter Your City Name.")
+    const apiKey = 'c1df8e9bc924ac36f435ab141f7c226d'
+    const cityName = document.getElementById("inputtext").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => getData(data))
+    
+    function getData(temp){
+        const countryName = temp.name;
+        console.log(countryName)
+        document.getElementById("cityShow").innerText = countryName;
     }
-    else{
-        let item = parseInt (document.getElementById("valueadd").value);
-        item = item +1
-        document.getElementById("valueadd").value = item;
-        document.getElementById("inputtext").value = "";   
-    }
+
 }
-
-
-    fetch('https://randomuser.me/api/?results=5000')
-        .then(res => res.json())
-        .then(data = console.log(data))
